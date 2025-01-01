6787300aaa4f28587e0558d564600a8e4e0cb3de
sirclex <lethanhluan2000@gmail.com>
2025-01-02 01:16:59 +0700

    feat: turn kafka on

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from api.v1 import api_router
 from core import settings, get_api_key
+from utils.kafka import consumer
+from logic.kafka_handler import handle_kafka_messages
 
 origins = {settings.ANALYTIC_CORS_ORIGIN}
 
@@ -13,3 +15,6 @@ app.add_middleware(
 )
     
 app.include_router(api_router, prefix="/myeco/analytic", dependencies=[Security(get_api_key)])
+
+for message in consumer:
+    handle_kafka_messages(message.topic, message.value)
