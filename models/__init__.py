8d69d34a3bedc5d2420124c076c0757cfb836085
sirclex <lethanhluan2000@gmail.com>
2025-01-02 00:41:31 +0700

    fix: not create facttransaction table

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,5 @@ from models.dim_category import DimCategory
 from models.dim_subcategory import DimSubcategory
 from models.dim_identity import DimIdentity
 from models.dim_status import DimStatus
+from models.fact_debt import FactDebt
+from models.fact_transaction import FactTransaction
--- models/base.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-
-class Base(DeclarativeBase):
-    def __init__(self, engine):
-        self.engine = engine
-
-    def create_all(self):
-        self.metadata.create_all(self.engine)
